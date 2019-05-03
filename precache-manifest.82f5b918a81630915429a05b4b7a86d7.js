86cb9ec022e8c6140945a709dc729dcf070c17e7
Koziel <tomasz.koziel@ubnt.com>
2019-05-03 16:46:55 +0200

    Updates

---
+++ precache-manifest.82f5b918a81630915429a05b4b7a86d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91f4d7b418eaa336781bc446e1778ba",
+    "url": "/game-of-life-ts/index.html"
+  },
+  {
+    "revision": "cfe2e8ae4599981ad922",
+    "url": "/game-of-life-ts/static/css/main.71d08fbd.chunk.css"
+  },
+  {
+    "revision": "20aa5a5a8bb788ca2826",
+    "url": "/game-of-life-ts/static/js/2.30c49cbe.chunk.js"
+  },
+  {
+    "revision": "cfe2e8ae4599981ad922",
+    "url": "/game-of-life-ts/static/js/main.1c461f84.chunk.js"
+  },
+  {
+    "revision": "a5d4a580524e7eb254f9",
+    "url": "/game-of-life-ts/static/js/runtime~main.427f075d.js"
+  }
+]);
