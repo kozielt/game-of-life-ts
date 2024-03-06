c6399f34a854fd01fcc2782d970d849d3280acf3
Tomasz Kozieł <tomasz@mutinyhq.com>
2024-03-06 12:45:04 +0100

    Update Header.tsx

--- src/sections/mgmt/Header.tsx
+++ src/sections/mgmt/Header.tsx
@@ -10,7 +10,7 @@ const containerCss = css`
 
 const Header: React.FC = () => (
   <div className={containerCss}>
-    <h1>Configuration</h1>
+    <h1>Configuration Deploy Worked</h1>
   </div>
 );
 
